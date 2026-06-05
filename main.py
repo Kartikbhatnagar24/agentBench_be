eb29a13a35ddaa439c497c3d350e75a0cc28c250
Kartik Bhatnagar <kartik@trumio.ai>
2026-06-05 11:42:38 +0530

    removed unused

--- main.py
+++ main.py
@@ -3,7 +3,6 @@ from fastapi.middleware.cors import CORSMiddleware
 from api.routers.auth import router as auth_router
 from api.routers.chat_session import router as chat_session_router
 from api.routers.upload import router as upload_router
-from api.routers.eval import router as eval_router
 from api.routers.analysis import router as analysis_router
 from rag_pipeline.qdrant import lifespan
 
@@ -20,7 +19,4 @@ app.add_middleware(
 app.include_router(auth_router)
 app.include_router(chat_session_router)
 app.include_router(upload_router)
-app.include_router(eval_router)
 app.include_router(analysis_router)
-
-
